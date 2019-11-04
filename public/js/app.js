c428ad6adaebb91b58456acb40eaf02d5475090f
René Maldonado <rmaldonado@MacBook-Pro-de-Rene.local>
2019-11-04 18:36:01 -0300

    change:model

--- public/js/app.js
+++ public/js/app.js
@@ -46751,8 +46751,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/osvaldo/sinader/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/osvaldo/sinader/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/rmaldonado/Proyectos/sinader/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/rmaldonado/Proyectos/sinader/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
